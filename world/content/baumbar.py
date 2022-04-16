88b8634f0a78777183619f632ab562fc8d433e2f
peddn <public@peddn.net>
2022-04-16 16:50:33 +0200

    ein test

--- world/content/baumbar.py
+++ world/content/baumbar.py
@@ -6,4 +6,4 @@ class BaumbarLounge(RoomIndoor):
     def at_object_creation(self):
         "This is called when object is first created, only."
         self.db.desc = 'Du stehst in der Lounge der Baumbar. Hier gibt es einige sehr gemütlich aussehnde Sofas. In Richtung Stammmitte befindet sich ein Durchgang, aus dessen Richtung es verführerisch nach gegritten Speisen riecht.'
-        # ein test
+        
