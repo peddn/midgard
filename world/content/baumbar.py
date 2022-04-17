92452685520630a8ceeeba2b4054de61f87a13f4
peddn <public@peddn.net>
2022-04-17 16:13:37 +0200

    baumbar update

--- world/content/baumbar.py
+++ world/content/baumbar.py
@@ -1,9 +1,12 @@
 from typeclasses.rooms.indoor import RoomIndoor
 
+from world.util.gender import Gender
+
 class BaumbarLounge(RoomIndoor):
     """
     """
     def at_object_creation(self):
         "This is called when object is first created, only."
         self.db.desc = 'Du stehst in der Lounge der Baumbar. Hier gibt es einige sehr gemütlich aussehnde Sofas. In Richtung Stammmitte befindet sich ein Durchgang, aus dessen Richtung es verführerisch nach gegritten Speisen riecht.'
-        
+        self.db.smell = 'Aus Richtung der Küche schwebt dir ein verführerischer Geruch nach gegrilltem Fleisch in die Nase'
+        self.db.gender = Gender.FEMININ
