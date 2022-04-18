4b491c01bb702cae56004cb34edccddb50312d38
peddn <public@peddn.net>
2022-04-18 20:22:46 +0200

    debug smell

--- commands/perception/cmds_perception.py
+++ commands/perception/cmds_perception.py
@@ -1,6 +1,7 @@
 from evennia import Command
 from world.util.grammar import Case, get_def_art
 
+from typeclasses.character import Character
 
 class CmdPerceptionSmell(Command):
     """
@@ -21,9 +22,15 @@ class CmdPerceptionSmell(Command):
                         caller.msg('Du riechst an dir.')
                         location.msg_contents('%s riecht an sich.' % (caller.key), exclude=caller)
                     else:
-                        caller.msg('Du riechst an %s %s.' % (get_def_art(target, Case.DAT), target.key))
+                        if isinstance(target, Character):
+                            caller.msg('Du riechst an %s.' % (target.key))
+                        else:
+                            caller.msg('Du riechst an %s %s.' % (get_def_art(target, Case.DAT), target.key))
                         target.msg('%s riecht an dir.' % (caller.key))
-                        location.msg_contents("%s riecht an %s." % (caller.key, target.key), exclude=[ caller, target ])
+                        if isinstance(target, Character):
+                            location.msg_contents("%s riecht an %s." % (caller.key, target.key), exclude=[ caller, target ])
+                        else:
+                            location.msg_contents("%s riecht an %s %s." % (get_def_art(target, Case.DAT), target.key), exclude=[ caller, target ])
                     caller.msg(target.return_smell(caller))
                 else:
                     caller.msg('Du riechst absolut gar nichts.')
