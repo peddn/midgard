bf738ef61fa6c4a2252abe63b19a61005595bfbd
peddn <public@peddn.net>
2022-04-21 18:59:03 +0200

    development soul

--- commands/soul/cmds_soul.py
+++ commands/soul/cmds_soul.py
@@ -36,14 +36,12 @@ class CmdSoulDeute(Command):
             self.args = [arg.strip() for arg in self.args]
             self.args = [arg.lower() for arg in self.args]
             if self.args[0] != 'auf':
-                caller.msg('möchtest du eventulell AUF etwas deuten?\n')
+                caller.msg('möchtest du eventulell AUF etwas deuten?')
             else:
                 if len(self.args) > 1:
                     target = caller.search(self.args[1])
                     if target:
                         caller.msg('Du deutest auf ' + target.key)
-                    else:
-                        caller.msg('So etwas wie ein %s gibt es hier nicht.' % (self.args[1]))
                 else:
                     caller.msg('Auf was möchtest du denn deuten?')
         else:
